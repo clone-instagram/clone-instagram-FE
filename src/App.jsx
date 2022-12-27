112f8d0b59f4f2a94a4ac7fb603ca6a2fdece0e2
Sang Uk Jung <83222288+JSU0318@users.noreply.github.com>
2022-12-27 20:47:22 +0900

    충돌해결 3차

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import "./main.css";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import PostPage from "./pages/PostPage";
+import PostPostPage from "./pages/PostPostPage";
 import CommentPage from "./pages/CommentPage";
-import LoginPage from "./pages/LoginPage";
-import SignupPage from "./pages/SignupPage";
+import PostLoginPage from "./pages/PostLoginPage";
+import PostSignUpPage from "./pages/PostSignUpPage";
+import PostEditPage from "./pages/PostEditPage";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/post" element={<PostPage />} />
-      <Route path="/edit/:id" element={<PostPage />} />
-      <Route path="/sign_up" element={<SignupPage />} />
-      <Route path="/sign_in" element={<LoginPage />} />
+      <Route path="/post" element={<PostPostPage />} />
+      <Route path="/edit/:id" element={<PostEditPage />} />
+      <Route path="/sign_up" element={<PostSignUpPage />} />
+      <Route path="/sign_in" element={<PostLoginPage />} />
       <Route path="/comment/:id" element={<CommentPage />} />
     </Routes>
   );
