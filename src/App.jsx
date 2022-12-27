4aa67ac5230148a8a09344cd623356e6fa7546dc
Superduper-India <sunyoung1542@gmail.com>
2022-12-27 22:38:43 +0900

    라우트 경로 팀원과 통일

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,12 @@ import PostEditPage from './pages/PostEditPage';
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<PostLoginPage />} />
+      <Route path="/home" element={<HomePage />} />
       <Route path="/comment/:id" element={<CommentPage />} />
       <Route path="/posts" element={<PostPostPage />} />
       <Route path="/edit/:id" element={<PostEditPage />} />
       <Route path="/signup" element={<PostSignUpPage />} />
-      <Route path="/login" element={<PostLoginPage />} />
     </Routes>
   );
 }
--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,12 +1,12 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 // import HomePage from "../pages/HomePage";
 // import DetailPage from "../pages/DetailPage";
-import PostPostPage from "../pages/PostPostPage";
-import PostLoginPage from "../pages/PostLoginPage";
-import PostSignUpPage from "../pages/PostSignUpPage";
-import PostEditPage from "../pages/PostEditPadge";
+import PostPostPage from '../pages/PostPostPage';
+import PostLoginPage from '../pages/PostLoginPage';
+import PostSignUpPage from '../pages/PostSignUpPage';
+import PostEditPage from '../pages/PostEditPadge';
 // import PostEditPage from "../pages/PostEditPadge";
 
 const Router = () => {
@@ -15,10 +15,10 @@ const Router = () => {
       <Routes>
         {/* <Route path="/" element={<HomePage />} /> */}
         {/* <Route path="/detail/:id" element={<DetailPage />} /> */}
-        <Route path="login" element={<PostLoginPage />} />
-        <Route path="signup" element={<PostSignUpPage />} />
-        <Route path="posts" element={<PostPostPage />} />
-        <Route path="edit/:id" element={<PostEditPage />} />
+        <Route path="/" element={<PostLoginPage />} />
+        <Route path="/signup" element={<PostSignUpPage />} />
+        <Route path="/posts" element={<PostPostPage />} />
+        <Route path="/edit/:id" element={<PostEditPage />} />
       </Routes>
     </BrowserRouter>
   );
